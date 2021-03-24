2e814c939b6cb4f76579ff79e46da0550472a13d
tanisha-garg <tanishagarg804@gmail.com>
2021-03-24 19:43:14 +0530

    Changes in BillingServiceImpl

--- src/main/java/com/cg/vegetable/mgmt/service/BillingServiceImpl.java
+++ src/main/java/com/cg/vegetable/mgmt/service/BillingServiceImpl.java
@@ -1,32 +1,45 @@
 package com.cg.vegetable.mgmt.service;
 
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.vegetable.mgmt.entities.BillingDetails;
+import com.cg.vegetable.mgmt.exceptions.BillNotFoundException;
+import com.cg.vegetable.mgmt.repository.IBillingRepository;
 
+@Service
 public class BillingServiceImpl implements IBillingService{
 	
-//	@Autowired
-//	EntityManager entityManager;
+	@Autowired
+	IBillingRepository billingRepository;
 
+	@Transactional
 	@Override
 	public BillingDetails addBill(BillingDetails bill) {
-//		entityManager.persist(bill);
-		return null;
+		billingRepository.save(bill);
+		return bill;
 	}
 
+	@Transactional
 	@Override
 	public BillingDetails updateBill(BillingDetails bill) {
-		// TODO Auto-generated method stub
-		return null;
+		bill = billingRepository.save(bill);
+		return bill;
 	}
 
 	@Override
 	public BillingDetails viewBill(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<BillingDetails> optional = billingRepository.findById(id);
+		if(!optional.isPresent()) {
+			throw new BillNotFoundException("Bill with id "+id+" doesn't exist");
+		}
+		BillingDetails bill = optional.get();
+		return bill;
 	}
 
 }
