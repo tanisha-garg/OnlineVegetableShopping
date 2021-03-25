d850d99b293fb54f274074e649fdcae681d9ff74
abhisheknishad <abhisheknishad08121998@gmail.com>
2021-03-26 00:18:58 +0530

    Tu added

--- src/main/java/com/cg/vegetable/mgmt/OnlinevegetableshoppingApplication.java
+++ src/main/java/com/cg/vegetable/mgmt/OnlinevegetableshoppingApplication.java
@@ -2,12 +2,17 @@ package com.cg.vegetable.mgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.cg.vegetable.mgmt.ui.OnlineVegetableShoppingUI;
 
 @SpringBootApplication
 public class OnlinevegetableshoppingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinevegetableshoppingApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(OnlinevegetableshoppingApplication.class, args);
+		OnlineVegetableShoppingUI onlineVegetableShoppingUI = context.getBean(OnlineVegetableShoppingUI.class);
+		onlineVegetableShoppingUI.start();
 	}
 
 }
