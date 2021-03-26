facfe4b32c62bf888a8379324928d198c6005692
abhisheknishad <abhisheknishad08121998@gmail.com>
2021-03-26 13:34:37 +0530

    Main method name changed

--- src/main/java/com/cg/vegetable/mgmt/OnlineVegetableShoppingApplication.java
+++ src/main/java/com/cg/vegetable/mgmt/OnlineVegetableShoppingApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.cg.vegetable.mgmt.ui.OnlineVegetableShoppingUI;
 
 @SpringBootApplication
-public class OnlinevegetableshoppingApplication {
+public class OnlineVegetableShoppingApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context=SpringApplication.run(OnlinevegetableshoppingApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(OnlineVegetableShoppingApplication.class, args);
 		OnlineVegetableShoppingUI onlineVegetableShoppingUI = context.getBean(OnlineVegetableShoppingUI.class);
 		onlineVegetableShoppingUI.start();
 	}
