7272376521dc9a05562751b65e00c5d03062da09
tanisha-garg <tanishagarg804@gmail.com>
2021-03-26 18:04:03 +0530

    Rest Controllers Updated

--- src/main/java/com/cg/vegetable/mgmt/controllers/BillingRestController.java
+++ src/main/java/com/cg/vegetable/mgmt/controllers/BillingRestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.cg.vegetable.mgmt.dto.BillingDetailsDTO;
+import com.cg.vegetable.mgmt.dto.ChangeTransactionStatusRequest;
+import com.cg.vegetable.mgmt.dto.CreateBillDetailsRequest;
 import com.cg.vegetable.mgmt.entities.BillingDetails;
 import com.cg.vegetable.mgmt.service.IBillingService;
 import com.cg.vegetable.mgmt.util.BillingDetailsUtil;
@@ -25,6 +27,21 @@ public class BillingRestController {
 		return details;
 	}
 	
+	@PutMapping("/changeStatus")
+	public BillingDetailsDTO changeTransactionStatus(@RequestBody ChangeTransactionStatusRequest requestData) {
+		BillingDetails bill = billingService.viewBill(requestData.getBillingId());
+		bill.setTransactionStatus(requestData.getTransactionStatus());
+		BillingDetails updatedBill = billingService.updateBill(bill);
+		BillingDetailsDTO details = billUtil.toDetails(updatedBill);
+		return  details;
+	}
 	
+	@PostMapping("/addBill")
+	public String addBill(@RequestBody CreateBillDetailsRequest requestData) {
+		BillingDetails bill = new BillingDetails(requestData.getOrderId(), requestData.getTransactionMode(), 
+													requestData.getTransactionStatus());
+		BillingDetails addedBill = billingService.addBill(bill);
+		return "Bill with id "+addedBill.getBillingId()+" created";
+	}
 
 }
---
+++ src/main/java/com/cg/vegetable/mgmt/dto/CreateBillDetailsRequest.java
@@ -0,0 +1,50 @@
+package com.cg.vegetable.mgmt.dto;
+
+import java.time.LocalDateTime;
+
+public class CreateBillDetailsRequest {
+	
+	private int billingId;
+	private int orderId;
+	private String transactionMode;
+	private String transactionStatus;
+	
+	public CreateBillDetailsRequest() {
+		
+	}
+
+	public int getBillingId() {
+		return billingId;
+	}
+
+	public void setBillingId(int billingId) {
+		this.billingId = billingId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getTransactionMode() {
+		return transactionMode;
+	}
+
+	public void setTransactionMode(String transactionMode) {
+		this.transactionMode = transactionMode;
+	}
+
+	public String getTransactionStatus() {
+		return transactionStatus;
+	}
+
+	public void setTransactionStatus(String transactionStatus) {
+		this.transactionStatus = transactionStatus;
+	}
+	
+	
+
+}
