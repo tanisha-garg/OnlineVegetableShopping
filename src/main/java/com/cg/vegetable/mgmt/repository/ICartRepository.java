d3df097fe8ac23e6553f0fb23b234391e66c55a3
tanisha-garg <tanishagarg804@gmail.com>
2021-03-27 11:40:50 +0530

    Added classes for Rest - Order Module

--- src/main/java/com/cg/vegetable/mgmt/repository/ICartRepository.java
+++ src/main/java/com/cg/vegetable/mgmt/repository/ICartRepository.java
@@ -2,14 +2,14 @@ package com.cg.vegetable.mgmt.repository;
 
 import java.util.List;
 
-import com.cg.vegetable.mgmt.entities.CartDTO;
+import com.cg.vegetable.mgmt.entities.Cart;
 import com.cg.vegetable.mgmt.entities.Vegetable;
 
 public interface ICartRepository {
 
 	public Vegetable addToCart(Vegetable item);
 	public Vegetable updateItemQuantity(Vegetable id,int quantity);
-	public List<Vegetable> viewAllItems(CartDTO cart);
-	public CartDTO removeAllItems(CartDTO cart);
+	public List<Vegetable> viewAllItems(Cart cart);
+	public Cart removeAllItems(Cart cart);
 	
 }
