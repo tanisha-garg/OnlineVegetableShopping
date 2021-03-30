e7166349a5322148071d2302c936e177d3f65fa8
abhisheknishad <abhisheknishad08121998@gmail.com>
2021-03-30 12:04:58 +0530

    Cart Dto added;

---
+++ src/main/java/com/cg/vegetable/mgmt/dto/RemoveAllVegetablesFromCart.java
@@ -0,0 +1,15 @@
+package com.cg.vegetable.mgmt.dto;
+
+public class RemoveAllVegetablesFromCart {
+	int cartId;
+
+	public int getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	
+
+}
