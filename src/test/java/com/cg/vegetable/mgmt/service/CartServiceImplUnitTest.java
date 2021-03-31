b3f04dfc6497c7e758511c7cafcec7b407854de0
Sameer9095 <sameermishra1999@gmail.com>
2021-03-31 11:59:09 +0530

    changes in unitTest

--- src/test/java/com/cg/vegetable/mgmt/service/CartServiceImplUnitTest.java
+++ src/test/java/com/cg/vegetable/mgmt/service/CartServiceImplUnitTest.java
@@ -17,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.cg.vegetable.mgmt.entities.Cart;
 import com.cg.vegetable.mgmt.entities.Customer;
 import com.cg.vegetable.mgmt.entities.Vegetable;
+import com.cg.vegetable.mgmt.exceptions.CartException;
 import com.cg.vegetable.mgmt.exceptions.CartIsEmptyException;
 import com.cg.vegetable.mgmt.exceptions.InvalidVegetableQuantityException;
 import com.cg.vegetable.mgmt.exceptions.VegValAboveZeroException;
@@ -51,14 +52,17 @@ public class CartServiceImplUnitTest {
 	
 	
 	/**
-	 * Scenario: item list is not null
+	 * Scenario: item list is not null,when items exist in cart before
 	 */
 	@Test
 	void addToCart_1() {
+		int custId=1;
+		int vegId = 2;
+		int quantity = 1;
 		Vegetable veg = Mockito.mock(Vegetable.class);
 		Cart cart = Mockito.mock(Cart.class);
 		Mockito.doNothing().when(cartService).validateVegetable(veg);
-		Mockito.doNothing().when(cartService.increaseVegQuantity(0, 0, 0));//suspected code
+		Mockito.doNothing().when(cartService.increaseVegQuantity(custId,vegId,quantity));//suspected code
 		Mockito.verify(cartService).increaseVegQuantity(1, 1, 1);
 		Mockito.verify(cartService).validateVegetable(veg);
 	}
@@ -66,4 +70,19 @@ public class CartServiceImplUnitTest {
 	/**
 	 * scenario  , cart is null
 	 */
+//	@Test
+//	void addToCart_2() {
+//		Vegetable veg = new Vegetable();
+//		Mockito.doThrow(CartException.class).when(cartService).validateVegetable(veg);
+//		Executable executable = () -> cartService.addToCart(0, veg);   ////suspected
+//	}
+	/**
+	 * Scenario: Removing all item
+	 */
+	@Test
+	void removeAllVegetables_1() {
+		Cart cart = Mockito.mock(Cart.class);
+		Mockito.doNothing().when(cartService).validateVegetable(null);
+	}
+	
 }
