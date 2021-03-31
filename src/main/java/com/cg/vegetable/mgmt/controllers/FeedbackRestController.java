2f6dd05b5a6a12581d4eca8d58203d0c064c120b
tanisha-garg <tanishagarg804@gmail.com>
2021-03-31 20:16:15 +0530

    Updating feedback

--- src/main/java/com/cg/vegetable/mgmt/controllers/FeedbackRestController.java
+++ src/main/java/com/cg/vegetable/mgmt/controllers/FeedbackRestController.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.vegetable.mgmt.dto.AddFeedbackDetailsRequest;
 import com.cg.vegetable.mgmt.dto.FeedbackDetails;
-import com.cg.vegetable.mgmt.entities.Customer;
 import com.cg.vegetable.mgmt.entities.Feedback;
-import com.cg.vegetable.mgmt.service.ICustomerService;
 import com.cg.vegetable.mgmt.service.IFeedbackService;
 import com.cg.vegetable.mgmt.util.FeedbackUtil;
 
@@ -25,29 +25,30 @@ public class FeedbackRestController {
 	@Autowired
 	private FeedbackUtil feedbackUtil;
 	
-	@Autowired
-	private ICustomerService customerService;
-	
 	@PostMapping ("/add")
-	public String addFeedback(@RequestBody FeedbackDetails requestData)
+	public FeedbackDetails addFeedback(@RequestBody AddFeedbackDetailsRequest requestData)
 	{
-		
-		Customer customer = customerService.viewCustomer(requestData.getCustId());
-		Feedback feedback = new Feedback(requestData.getFeedbackId(),requestData.getVegetableId(),requestData.getRating(),requestData.getComment());
+		Feedback feedback = new Feedback();
+		feedback.setCustId(requestData.getCustId());
+		feedback.setVegetableId(requestData.getVegetableId());
+		feedback.setRating(requestData.getRating());
+		feedback.setComments(requestData.getComment());		
 		Feedback feedbackAdded = feedbackService.addFeedback(feedback);
-		return "Feedback added successfully for customer ID"+feedbackAdded.getCustId();
+		FeedbackDetails details = feedbackUtil.toDetail(feedbackAdded);
+		return details;
 	}
 	
-	@GetMapping("/viewAll")
-	public List<FeedbackDetails> fetchAllFeedbacks(@RequestBody FeedbackDetails requestData) {
-		List<Feedback> feedbackList = feedbackService.viewAllFeedbacks(requestData.getVegetableId());
+	@GetMapping("/view/veg/{id}")
+	public List<FeedbackDetails> fetchAllFeedbacks(@PathVariable("id") int vegId) {
+		List<Feedback> feedbackList = feedbackService.viewAllFeedbacks(vegId);
 		List<FeedbackDetails> allFeedbackDetails = feedbackUtil.toFeedbackDetails(feedbackList);
 		return allFeedbackDetails;
-			}
+		
+	}
 	
-	@GetMapping("/view")
-	public List<FeedbackDetails> fetchFeedbacks(@RequestBody FeedbackDetails requestData) {
-		List<Feedback> feedbackList = feedbackService.viewFeedbacks(requestData.getCustId());
+	@GetMapping("/view/customer/{id}")
+	public List<FeedbackDetails> fetchFeedbacks(@PathVariable("id") int customerId) {
+		List<Feedback> feedbackList = feedbackService.viewFeedbacks(customerId);
 		List<FeedbackDetails> feedbackDetails = feedbackUtil.toFeedbackDetails(feedbackList);
 		return feedbackDetails;
 			}
--- src/main/java/com/cg/vegetable/mgmt/dto/AddFeedbackDetailsRequest.java
+++ src/main/java/com/cg/vegetable/mgmt/dto/AddFeedbackDetailsRequest.java
@@ -3,13 +3,37 @@ package com.cg.vegetable.mgmt.dto;
 import com.cg.vegetable.mgmt.entities.Feedback;
 
 public class AddFeedbackDetailsRequest {
- private Feedback fdbk;
+	
+	private int custId;
+	private int vegetableId;
+	private int rating;
+	private String comment;
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public int getVegetableId() {
+		return vegetableId;
+	}
+	public void setVegetableId(int vegetableId) {
+		this.vegetableId = vegetableId;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 
-public Feedback getFdbk() {
-	return fdbk;
-}
-
-public void setFdbk(Feedback fdbk) {
-	this.fdbk = fdbk;
-}
 }
