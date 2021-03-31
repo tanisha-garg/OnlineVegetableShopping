05793a13112a2e8d4289c256a08e6fe6a2c4c739
tanisha-garg <tanishagarg804@gmail.com>
2021-03-31 13:24:45 +0530

    cart service impl unit test

--- src/test/java/com/cg/vegetable/mgmt/controllers/BillingDetailsRestUnitTest.java
+++ src/test/java/com/cg/vegetable/mgmt/controllers/BillingDetailsRestUnitTest.java
@@ -1,5 +1,28 @@
 package com.cg.vegetable.mgmt.controllers;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.cg.vegetable.mgmt.service.IBillingService;
+import com.cg.vegetable.mgmt.util.BillingDetailsUtil;
+
+@ExtendWith(MockitoExtension.class)
 public class BillingDetailsRestUnitTest {
+	
+	@Mock
+	IBillingService billingService;
+	
+	@Mock
+	BillingDetailsUtil billingDetailsUtil;
+	
+	@Spy
+	@InjectMocks
+	BillingRestController billController;
+	
+	
 
 }
