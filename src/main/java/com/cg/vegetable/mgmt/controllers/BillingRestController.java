c9b7b79ce810fc0a5e5676d7671349ffb6fd38f5
tanisha-garg <tanishagarg804@gmail.com>
2021-04-01 10:24:55 +0530

    Added validated and valid

--- src/main/java/com/cg/vegetable/mgmt/controllers/BillingRestController.java
+++ src/main/java/com/cg/vegetable/mgmt/controllers/BillingRestController.java
@@ -1,6 +1,11 @@
 package com.cg.vegetable.mgmt.controllers;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.cg.vegetable.mgmt.dto.BillingDetailsResponse;
@@ -11,6 +16,7 @@ import com.cg.vegetable.mgmt.entities.BillingDetails;
 import com.cg.vegetable.mgmt.service.IBillingService;
 import com.cg.vegetable.mgmt.util.BillingDetailsUtil;
 
+@Validated
 @RequestMapping("/bills")
 @RestController
 public class BillingRestController {
@@ -22,14 +28,14 @@ public class BillingRestController {
     private BillingDetailsUtil billUtil;
 
     @GetMapping(value = "get/{id}")
-    public BillingDetailsResponse fetchBillDetails(@PathVariable("id") int id) {
+    public BillingDetailsResponse fetchBillDetails(@PathVariable("id") @Min(1) int id) {
         BillingDetails bill = billingService.viewBill(id);
         return billUtil.toDetails(bill);
     }
 
     @PutMapping("/update/status/{id}")
-    public BillingDetailsResponse updateTransactionStatus(@RequestBody UpdateTransactionStatusRequest requestData,
-    												@PathVariable("id") int id) {
+    public BillingDetailsResponse updateTransactionStatus(@RequestBody @Valid UpdateTransactionStatusRequest requestData,
+    												@PathVariable("id") @Min(1) int id) {
         BillingDetails bill = billingService.viewBill(id);
         bill.setTransactionStatus(requestData.getTransactionStatus());
         BillingDetails updatedBill = billingService.updateBill(bill);
@@ -37,16 +43,17 @@ public class BillingRestController {
     }
     
     @PutMapping("/update/mode/{id}")
-    public BillingDetailsResponse updateTransactionMode(@RequestBody UpdateTransactionModeRequest requestData,
-    													@PathVariable("id") int id) {
+    public BillingDetailsResponse updateTransactionMode(@RequestBody @Valid UpdateTransactionModeRequest requestData,
+    													@PathVariable("id") @Min(1) int id) {
     	BillingDetails bill = billingService.viewBill(id);
     	bill.setTransactionMode(requestData.getTransactionMode());
     	BillingDetails updatedBill = billingService.updateBill(bill);
     	return billUtil.toDetails(updatedBill);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/add")
-    public BillingDetailsResponse addBill(@RequestBody AddBillDetailsRequest requestData) {
+    public BillingDetailsResponse addBill(@RequestBody @Valid AddBillDetailsRequest requestData) {
         BillingDetails bill = new BillingDetails(requestData.getOrderId(), requestData.getTransactionMode(),
                 requestData.getTransactionStatus());
         BillingDetails addedBill = billingService.addBill(bill);
